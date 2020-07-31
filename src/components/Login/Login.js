bb3b0dac3a8dad0208f909a5312b00831e6acd23
sacc <50913285+filipkmiecik@users.noreply.github.com>
2020-07-31 02:00:26 +0200

    added text input change handlers

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,31 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./Login.module.css";
 
-const Login = (props) => (
-  <React.Fragment>
-    <div className={styles.Container}>
-      <p className={styles.Greeting}>Hey there. Good to see you again.</p>
-    </div>
-    <div className={styles.Container}>
-      <p className={styles.Error}></p>
-    </div>
-    <form>
+const Login = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const changeHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
+  return (
+    <React.Fragment>
       <div className={styles.Container}>
-        <label>
-          E-MAIL <input className={styles.TextInput} type="text" />
-        </label>
+        <p className={styles.Greeting}>Hey there. Good to see you again.</p>
       </div>
       <div className={styles.Container}>
-        <label>
-          PASSWORD <input className={styles.TextInput} type="text" />
-        </label>
+        <p className={styles.Error}>{errorMessage}</p>
       </div>
-      <div className={styles.Container}>
-        <button>SIGN IN</button>
-      </div>
-    </form>
-  </React.Fragment>
-);
+      <form>
+        <div className={styles.Container}>
+          <label>
+            E-MAIL{" "}
+            <input
+              className={styles.TextInput}
+              type="text"
+              value={email}
+              onChange={changeHandler}
+            />
+          </label>
+        </div>
+        <div className={styles.Container}>
+          <label>
+            PASSWORD{" "}
+            <input className={styles.TextInput} type="text" value={password} />
+          </label>
+        </div>
+        <div className={styles.Container}>
+          <button className={styles.SubmitButton}>SIGN IN</button>
+        </div>
+      </form>
+      <div className={styles.Container}>Don't have an account yet? Sign Up</div>
+    </React.Fragment>
+  );
+};
 
 export default Login;
